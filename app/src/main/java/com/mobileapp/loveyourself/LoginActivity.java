875a3de659302749072ddbf6a00410ad468d1525
Nico Villegas <john@tagcash.com>
2017-03-10 17:47:30 +0800

    Initial Commit

---
+++ app/src/main/java/com/mobileapp/loveyourself/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.mobileapp.loveyourself;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nico on 3/10/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
