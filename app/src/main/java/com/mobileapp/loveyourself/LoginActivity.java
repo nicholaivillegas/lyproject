3e3cb5052c7b9e059009a07a7f551c7ee9823fcb
Nico Villegas <john@tagcash.com>
2017-03-13 15:57:24 +0800

    Todo: fb login

--- app/src/main/java/com/mobileapp/loveyourself/LoginActivity.java
+++ app/src/main/java/com/mobileapp/loveyourself/LoginActivity.java
@@ -16,21 +16,33 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "LOGIN: ";
+    private static final int RC_SIGN_IN = 1;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    Button btnLogin, btnAnon, btnForgot, btnSignUp;
+    Button btnLogin, btnForgot, btnSignUp;
+    SignInButton btnGoogle;
     EditText etEmail, etPassword;
     TextInputLayout tilEmail, tilPassword;
     ProgressBar progress;
+    GoogleApiClient mGoogleApiClient;
 
 
     @Override
@@ -38,6 +50,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnLogin = (Button) findViewById(R.id.btn_login);
+        btnGoogle = (SignInButton ) findViewById(R.id.btn_google);
         btnForgot = (Button) findViewById(R.id.btn_reset_password);
         btnSignUp = (Button) findViewById(R.id.btn_signup);
         etEmail = (EditText) findViewById(R.id.edit_email);
@@ -53,7 +66,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 FirebaseUser user = firebaseAuth.getCurrentUser();
 
                 if (user != null) {
-                    if (user.isEmailVerified() || (user.isAnonymous())) {
+                    if (user.isEmailVerified()) {
                         // User is signed in
                         Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
                         Intent i = new Intent(LoginActivity.this, MainActivity.class);
@@ -102,6 +115,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         });
         btnLogin.setOnClickListener(this);
+        btnGoogle.setOnClickListener(this);
         btnForgot.setOnClickListener(this);
         btnSignUp.setOnClickListener(this);
     }
@@ -139,6 +153,60 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    public void gSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
     public void verifyEmail() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         user.sendEmailVerification()
@@ -159,6 +227,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.btn_login:
                 progress.setVisibility(View.VISIBLE);
                 signInEmailPassword(etEmail.getText().toString(), etPassword.getText().toString());
+                break;
+
+            case R.id.btn_google:
+                gSignIn();
                 break;
 
             case R.id.btn_reset_password:
@@ -185,6 +257,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         } else {
             btnLogin.setEnabled(true);
         }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
 
